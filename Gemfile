fa73d1eafea48ca1237ef247bd313edf314afc3a
Uchio Kondo <udzura@udzura.jp>
2024-10-23 00:15:18 +0900

    Initialize types

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ gem "rake", "~> 13.0"
 gem "test-unit", "~> 3.0"
 
 gem "rbs-inline", "~> 0.9.0", :require => false
+gem "steep", ">= 1.8.1"
--- lib/waru.rb
+++ lib/waru.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
+# rbs_inline: enabled
 
 require_relative "waru/version"
 
 module Waru
+  module BinaryLoader
+    # @rbs buf: IO::Buffer
+    # @rbs return: Instance
+    def self.load_from_buffer(buf)
+      return Instance.new
+    end
+  end
+
+  class Instance
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
